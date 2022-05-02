a0a27a81ab47461e591f366df19eaf74ab3d5486
amuenzenmay <augiemuenz@gmail.com>
2022-05-02 15:38:04 -0500

    Bumpyness Calculation

--- tetrisAgent.py
+++ tetrisAgent.py
@@ -2,15 +2,56 @@ from tetris_model import BOARD_DATA, Shape
 import math
 from datetime import datetime
 import numpy as np
+import random
+
 
 class gameState:
-    def __init__(self, shape, direction, nextShape, board):
+    def __init__(self, holes, board, shape):
         self.shape = shape
-        self.direction = direction
-        self.nextShape = nextShape
+        self.holes = holes
         self.board = board
+
 
 class TetrisAI(object):
     def nextMove(self):
         if BOARD_DATA.nextShape == Shape.shapeNone:
             return None
+
+        if BOARD_DATA.currentShape.shape in (Shape.shapeI, Shape.shapeZ, Shape.shapeS):
+            currDirRange = [0, 1]
+        elif BOARD_DATA.currentShape.shape == Shape.shapeO:
+            currDirRange = [0, ]
+        else:
+            currDirRange = [0, 1, 2, 3]
+
+        if BOARD_DATA.nextShape.shape in (Shape.shapeI, Shape.shapeZ, Shape.shapeS):
+            nextDirRange = [0, 1]
+        elif BOARD_DATA.nextShape.shape == Shape.shapeO:
+            nextDirRange = [0, ]
+        else:
+            nextDirRange = [0, 1, 2, 3]
+
+        random.shuffle(currDirRange)
+        randDir = currDirRange[0]
+
+        minX, maxX, minY, maxY = BOARD_DATA.currentShape.getBoundingOffsets(randDir)
+        validX = list(range(minX, maxX + 1))
+        random.shuffle(validX)
+        randX = validX[0]
+        print(self.bumpyness(BOARD_DATA.currentShape, BOARD_DATA.currentDirection, randX))
+
+        return (randDir, randX, 0)
+
+    def bumpyness(self, shape, direction, x0):
+        dy = BOARD_DATA.height - 1
+        board = np.array(BOARD_DATA.getData()).reshape((BOARD_DATA.height, BOARD_DATA.width))
+        bumpyness = [0] * BOARD_DATA.width
+        for col in range(BOARD_DATA.width):
+            for row in range(0, BOARD_DATA.height):
+                if board[row, col]:
+                    if row < bumpyness[col]:
+                        bumpyness[col] = row
+        return bumpyness
+
+
+TETRIS_AI = TetrisAI()
--- tetris_game.py
+++ tetris_game.py
@@ -7,7 +7,8 @@ from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
 
 from tetris_model import BOARD_DATA, Shape
-from tetris_ai import TETRIS_AI
+# from tetris_ai import TETRIS_AI
+from tetrisAgent import TETRIS_AI
 
 # TETRIS_AI = None
 
@@ -24,7 +25,7 @@ class Tetris(QMainWindow):
 
     def initUI(self):
         self.gridSize = 22
-        self.speed = 0.5
+        self.speed = 1000
 
         self.timer = QBasicTimer()
         self.setFocusPolicy(Qt.StrongFocus)
